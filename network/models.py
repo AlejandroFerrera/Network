8bec2658b77efdc923860620151752db91b8e248
Alejandro Ferrera Hernandez <aferrerah1421@gmail.com>
2022-12-18 22:10:26 -0300

    Add Post model

--- network/models.py
+++ network/models.py
@@ -6,3 +6,12 @@ class User(AbstractUser):
     
     avatar_img = models.URLField()
     following = models.ManyToManyField('self', related_name='followers')
+
+class Post(models.Model):
+
+    owner = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
+    content = models.CharField(max_length=200, blank=False, null=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+
+
