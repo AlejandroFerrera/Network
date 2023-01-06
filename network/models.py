aa5ce9cb8a5390b7e2d70928b30a06b23165fe9e
Alejandro Ferrera Hernandez <aferrerah1421@gmail.com>
2023-01-05 22:59:15 -0300

    Add some unit tests with setup

--- network/models.py
+++ network/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class User(AbstractUser):
@@ -12,6 +14,9 @@ class Post(models.Model):
     owner = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
     content = models.CharField(max_length=200, blank=False, null=False)
     created_date = models.DateTimeField(auto_now_add=True)
-    likes = models.PositiveIntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+
+
 
 
--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import User, Post
 
-# Create your tests here.
+
+class CreatePostTestCase(TestCase):
+
+    def setUp(self):
+
+        # Create Dummy User
+        user = User.objects.create(username='TestUser')
+
+        # Create Dummy Post
+        post = Post.objects.create(
+            owner=user, content='This is a test content')
+
+    def test_user_posts_count(self):
+        u = User.objects.get(username='TestUser')
+        self.assertEqual(u.posts.count(), 1)
+
+    def test_post_owner_count(self):
+        p = Post.objects.get(content='This is a test content')
+        self.assertTrue(p.owner, 'No puede tener mas de un dueño el post')
+
+    def test_post_valid_likes(self):
+        p = Post.objects.get(content = 'This is a test content')
+        self.assertTrue(p.likes >= 0)
+    
+    
