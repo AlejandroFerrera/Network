b223ffd238ed576f5aa305de1b2b404e018d8062
Alejandro Ferrera Hernandez <aferrerah1421@gmail.com>
2023-01-12 21:08:14 -0300

    Refactor tests

--- network/tests.py
+++ network/tests.py
@@ -5,29 +5,28 @@ from .models import User, Post
 class CreatePostTestCase(TestCase):
 
     def setUp(self):
-
-        # Create Dummy User
-        user = User.objects.create(username='TestUser')
-
-        # Create Dummy Post
-        post = Post.objects.create(
-            owner=user, content='This is a test content')
+        self.client = Client()
+        self.user = User.objects.create(
+            username = 'testuser',
+            password = 'testpassword'
+        )
 
     def test_user_posts_count(self):
-        u = User.objects.get(username='TestUser')
-        self.assertEqual(u.posts.count(), 1)
+        p = Post.objects.create(content = 'test content', owner = self.user)
+        self.assertEqual(self.user.posts.count(), 1)
 
     def test_assign_owner(self):
-        p = Post.objects.get(content='This is a test content')
-        u = User.objects.get(username='TestUser')
-        self.assertTrue(p.owner, u)
+        p = Post.objects.create(content='This is a test content', owner = self.user)
+        self.assertTrue(p.owner, self.user)
+        self.assertTrue(p.content, 'This is a test content')
 
     def test_post_valid_likes(self):
-        p = Post.objects.get(content = 'This is a test content')
+        p = Post.objects.create(content = 'This is a test content', owner = self.user)
         self.assertTrue(p.likes >= 0)
     
     def test_index(self):
         c = Client()
+        p = Post.objects.create(content = '', owner = self.user)
         response = c.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["posts"].count(), 1)
