356f095dacbcc335766c0b5ccb05c36ee65d0843
AlejandroFerrera <aferrerah1421@gmail.com>
2023-07-21 23:56:54 -0400

    User profile changes

--- network/models.py
+++ network/models.py
@@ -5,16 +5,23 @@ import datetime
 
 
 class User(AbstractUser):
-    
-    avatar_img = models.URLField()
-    following = models.ManyToManyField('self', related_name='followers', blank=True, symmetrical=False)
+	
+	avatar_img = models.URLField()
+	following = models.ManyToManyField('self', related_name='followers', blank=True, symmetrical=False)
+	
+	def get_number_of_followers(self):
+		return self.followers.count()
+
+	def get_number_of_following(self):
+		return self.following.count()
+
 
 class Post(models.Model):
 
-    owner = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
-    content = models.CharField(max_length=200, blank=False, null=False)
-    created_date = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField(default=0)
+	owner = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
+	content = models.CharField(max_length=200, blank=False, null=False)
+	created_date = models.DateTimeField(auto_now_add=True)
+	likes = models.IntegerField(default=0)
 
 
 
--- network/views.py
+++ network/views.py
@@ -9,75 +9,85 @@ from .models import User, Post
 
 def index(request):
 
-    if request.method == 'POST':
+	if request.method == 'POST':
 
-        if not request.user.is_authenticated:
-            return redirect('index')
+		if not request.user.is_authenticated:
+			return redirect('index')
 
-        content = request.POST['new_post_content']
-        Post.objects.create(owner = request.user, content = content)
+		content = request.POST['new_post_content']
+		Post.objects.create(owner = request.user, content = content)
 
-    posts = Post.objects.all().order_by('-created_date')
-    return render(request, "network/pages/index.html", {'posts': posts})
+	posts = Post.objects.all().order_by('-created_date')
+	return render(request, "network/pages/index.html", {'posts': posts})
 
 def user_profile_view(request, username):
-    
+
+	user = None
+	
 	try:
-		user = User.objects.get(username=username)
+		user = User.objects.get(username=username)	
 	except User.DoesNotExist:
 		return render(request, "network/pages/404.html")
+	
+	number_of_followers = user.get_number_of_followers()
+	number_of_following = user.get_number_of_following()
+	user_posts = user.posts.all().order_by('-created_date')
 
-	return render(request, "network/pages/user_profile.html", {'user': user})
-
+	return render(request, "network/pages/user_profile.html", {
+		'user': user,
+		'number_of_followers': number_of_followers,
+		'number_of_following': number_of_following,
+		'posts': user_posts
+	})
 # AUTH
 def login_view(request):
-    if request.method == "POST":
+	if request.method == "POST":
 
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+		# Attempt to sign user in
+		username = request.POST["username"]
+		password = request.POST["password"]
+		user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "network/pages/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "network/pages/login.html")
+		# Check if authentication successful
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect(reverse("index"))
+		else:
+			return render(request, "network/pages/login.html", {
+				"message": "Invalid username and/or password."
+			})
+	else:
+		return render(request, "network/pages/login.html")
 
 
 def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
+	logout(request)
+	return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
+	if request.method == "POST":
+		username = request.POST["username"]
+		email = request.POST["email"]
 
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "network/pages/register.html", {
-                "message": "Passwords must match."
-            })
+		# Ensure password matches confirmation
+		password = request.POST["password"]
+		confirmation = request.POST["confirmation"]
+		if password != confirmation:
+			return render(request, "network/pages/register.html", {
+				"message": "Passwords must match."
+			})
 
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "network/pages/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "network/pages/register.html")
+		# Attempt to create new user
+		try:
+			user = User.objects.create_user(username, email, password)
+			user.save()
+		except IntegrityError:
+			return render(request, "network/pages/register.html", {
+				"message": "Username already taken."
+			})
+		login(request, user)
+		return HttpResponseRedirect(reverse("index"))
+	else:
+		return render(request, "network/pages/register.html")
 # END AUTH
